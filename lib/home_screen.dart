50943f9a489dd3767e65883e0fcd1a6c2f574bc7
Mustafa Musa <Msm.musaa@gmail.com>
2024-07-19 00:24:02 +0300

    inti project Islami app

---
+++ lib/home_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const String routeName = 'HomeScreen';
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Text(
+          'Islami',
+          style: TextStyle(
+              color: Color.fromRGBO(36, 36, 36, 1),
+              fontSize: 30,
+              fontWeight: FontWeight.w700,
+              height: 46.89),
+        ),
+        centerTitle: true,
+      ),
+    );
+  }
+}
